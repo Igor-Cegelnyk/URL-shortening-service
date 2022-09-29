7ab95bd0073acd2b8cab434e76351186570f3ce8
igor <il.cegelnik@gmail.com>
2022-09-29 22:51:29 +0300

    Added url model & views UrlViewSet

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
+import base64
+import uuid
+
 from django.db import models
 
-# Create your models here.
+HOST_NAME = 'http://localhost:8000/'
+
+
+class Url(models.Model):
+    url = models.EmailField(unique=True)
+    url_hash = models.CharField(max_length=10, unique=True, db_index=True)
+    short_url = models.EmailField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.url_hash = self.generate_hash()
+        self.short_url = self.create_short_url()
+        super(Url, self).save(*args, **kwargs)
+
+    def generate_hash(self):
+        hash_ = base64.urlsafe_b64encode(uuid.uuid4().bytes)[:6]
+        hash_exist = Url.objects.filter(url_hash=hash_)
+        while hash_exist:
+            hash_ = base64.urlsafe_b64encode(uuid.uuid4().bytes)[:6]
+            hash_exist = Url.objects.filter(url_hash=hash_)
+            continue
+        hash_ = hash_.decode('utf-8')
+
+        return hash_
+
+    def create_short_url(self):
+        return HOST_NAME + self.url_hash
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from app.models import Url
+
+
+class UrlViewSet(viewsets.ModelViewSet):
+    queryset = Url.objects.all()
+    serializer_class = CinemaHallSerializer
